9a6e333de7a84426671fa0d6a18c595210472a5c
dang.tran1 <dang.tran1@ptnglobalcorp.com>
2025-07-08 09:50:40 +0700

    add key value store

--- src/app/shared/utils.py
+++ src/app/shared/utils.py
@@ -1,8 +1,10 @@
 import json
+import pathlib
 import time
 from datetime import datetime
 
 from app import logger
+
 from ..paths import DATA_PATH
 
 
@@ -40,3 +42,42 @@ def load_delivery_method_list_mapping() -> dict:
 def load_product_mapping() -> dict:
     with open(DATA_PATH / "product_mapping.json") as f:
         return json.load(f)
+
+
+class KeyValueStore:
+    def __init__(self, name: str, save_dir: pathlib.Path) -> None:
+        self.name: str = name
+        self.save_dir: pathlib.Path = save_dir
+        self.data: dict[str, str] = {}
+
+        save_file = self.get_save_file()
+        if save_file.exists():
+            self.load_data()
+        else:
+            self.write_data()
+
+    def get_save_file(self) -> pathlib.Path:
+        return self.save_dir / f"{self.name}.json"
+
+    def load_data(self) -> None:
+        with open(self.get_save_file()) as f:
+            self.data = json.load(f)
+
+    def write_data(self) -> None:
+        with open(self.get_save_file(), "w") as f:
+            json.dump(self.data, f)
+
+    def get(self, key: str) -> str | None:
+        self.load_data()
+        return self.data.get(key)
+
+    def set(self, key: str, value: str) -> None:
+        self.data[key] = value
+        self.write_data()
+
+    def update(self, key: str, value: str) -> None:
+        self.set(key, value)
+
+    def delete(self, key: str) -> None:
+        del self.data[key]
+        self.write_data()

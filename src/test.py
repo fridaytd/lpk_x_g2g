473842cd17458f3cbdf5372021ac9608ea5d339d
thdangtr <thdangtr@gmail.com>
2025-07-29 22:33:44 +0700

    fix: Update function import to load product mapping and rates from sheet

--- src/test.py
+++ src/test.py
@@ -1,11 +1,12 @@
 import json
 
-from app.server.utils import load_product_mapping_from_sheet
+from app.server.utils import load_product_mapping_and_rates_from_sheet
 from app import config
 
 print(
     json.dumps(
-        load_product_mapping_from_sheet(config.SHEET_ID, config.MAPPING_SHEET_NAME, 2),
-        indent=4,
+        load_product_mapping_and_rates_from_sheet(
+            config.SHEET_ID, config.MAPPING_SHEET_NAME, 2
+        ),
     )
 )
